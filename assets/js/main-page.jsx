bfa2ccbfe75b72c16b5cbf7362e81370183357b8
tina-n219 <noorani.t@husky.neu.edu>
2018-11-20 15:23:57 -0500

    gets user location with geolocation

--- assets/js/main-page.jsx
+++ assets/js/main-page.jsx
@@ -3,32 +3,40 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import $ from 'jquery';
 import { Provider, connect } from 'react-redux';
+import {geolocated} from 'react-geolocated';
 
 import api from './api';
 import store from './store';
 
+
 class MainPage extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        location: this.getUserLoc(),
+        restaurants: [],
+      }
+    }
+
+    onPositionRecieved(position) {
+      console.log(position);
+    }
+
+    getUserLoc() {
+      var loc = navigator.geolocation.getCurrentPosition(this.onPositionRecieved)
+      this.setState({
+        location: loc,
+      });
     }
     
-
     render() {
-        console.log("it's rendering!");
+      if (!navigator.geolocation){
+        return <p>Geolocation is not supported by your browser</p>;
+      }
         return <div>
-          
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-    </div>
-    <div class="col-sm">
-      <h1>Welcome to Food Finder!</h1>
-    </div>
-    <div class="col-sm">
-    </div>
-  </div>
-</div>
-          </div>
+                <h1> This is where the magic happens </h1>
+                {this.state.location}
+            </div>
             }
     }
 
