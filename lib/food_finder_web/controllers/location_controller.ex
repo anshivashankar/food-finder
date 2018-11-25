0d98ce5872382d45fde4b8d9c3e58ab16d696e2f
tina-n219 <noorani.t@husky.neu.edu>
2018-11-25 11:28:54 -0500

    location controller adjustment

--- lib/food_finder_web/controllers/location_controller.ex
+++ lib/food_finder_web/controllers/location_controller.ex
@@ -14,7 +14,7 @@ defmodule FoodFinderWeb.LocationController do
     <> to_string(long) <> "&"
     <> "radius=5000&"
     <> "type=restaurant&"
-    <> "key=AIzaSyA8lySjDjHR1VVN2OT3fOB9w-NMS-MudjQ" 
+    <> "key=" 
     <> System.get_env("GOOGLE_API_KEY")
 
     #IO.inspect(url)
