438b37b2b6fd667928868cf924d45de1bc4097a1
James Wu <wu.jame@husky.neu.edu>
2018-11-25 21:52:59 -0500

    add foreign key constraint

--- lib/food_finder/friends/friend.ex
+++ lib/food_finder/friends/friend.ex
@@ -5,14 +5,19 @@ defmodule FoodFinder.Friends.Friend do
 
 
   schema "friends_with" do
-    field :primary_user_id, :id
-    field :secondary_user_id, :id
+    #field :primary_user_id, :id
+    #field :secondary_user_id, :id
+    belongs_to :primary_user, User
+    belongs_to :secondary_user, User
+
     timestamps()
   end
 
   @doc false
   def changeset(friend, attrs) do
     friend
+    |> foreign_key_constraint(:primary_user_id)
+    |> foreign_key_constraint(:secondary_user_id)
     |> cast(attrs, [:primary_user_id, :secondary_user_id])
     |> validate_required([:primary_user_id, :secondary_user_id])
   end
