681dd05ea5d570c3cfe05c6b511d97b7fc2841bb
tina-n219 <noorani.t@husky.neu.edu>
2018-11-26 21:43:33 -0500

    restaurant review filtering works

--- assets/js/RestaurantRatings.jsx
+++ assets/js/RestaurantRatings.jsx
@@ -16,13 +16,85 @@ class RestaurantRatings extends React.Component {
   }
 
   render() {
+    const user_id = localStorage.getItem("user_id");
+    const user_name = localStorage.getItem("user_name");
+
+    let { session, users, ratings } = this.props;
+
+    const usersReviews = review => {
+      return review.user_id == user_id ? review : null;
+    };
+
+    let allReviews = _.map(ratings, r => {
+      return (
+        <Rating
+          key={r.restaurant_id}
+          rating={r}
+          users={users}
+          session={session}
+        />
+      );
+    });
+
+    //console.log("Ratings ", allReviews);
+
+    // let users_reviews = _.map(allReviews, review =>
+    //   review.key == user_id ? review : "null"
+    // );
+
     return (
       <div>
-        <p> bitch</p>
+        <div class="container">
+          <div className="row">{allReviews}</div>{" "}
+        </div>
       </div>
     );
   }
 }
+
+class Rating extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: this.props.rating.name,
+      restaurant_id: this.props.rating.restaurant_id,
+      user_id: this.props.rating.user_id,
+      comment_text: this.props.rating.comment_text,
+      rating_number: this.props.rating.rating_number
+    };
+  }
+
+  render() {
+    let {rating, users, session} = this.props;
+    const user_loggedin = localStorage.getItem("user_id");
+
+    const { user_id, comment_text, rating_number, name, restaurant_id } = this.state;
+
+    let rest_id = localStorage.getItem("restaurant_id");
+  
+    if (restaurant_id == rest_id) {
+      return (
+        <div className="card col-4 ratings-card">
+          <div className="card-body">
+            <div className="form-group">
+              <h2 class="card-title">Review</h2>
+              <h5 class="card-text">{name}</h5>
+              <h5 class="card-text">Your thoughts: </h5>
+              <p>{comment_text}</p>
+              <h5 class="card-text">Your rating: </h5>
+              <p>{rating_number}</p>
+              <button class="btn btn-danger" onClick={() => api.delete_review(rating.id)}>Delete Review</button>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  }
+}
+
 
 function state2props(state) {
   return {
